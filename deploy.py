650e9aedd02c6bd73aab2bd23e0fbec6e795b779
TakayaTsunami <takaya.tsunami96@gmail.com>
2018-01-08 00:51:43 +0900

    firstCommit

--- deploy.py
+++ deploy.py
@@ -1,5 +1,5 @@
-import subprocess
-res = subprocess.call('ls')
-#=> lsコマンドの結果
-print res
-#=> コマンドが成功していれば 0
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+print "Content-Type: text/html\n"
+print "<html><body>Hello world!</body></html>"
